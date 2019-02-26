698275aa18deb75afc7039866b573eeca6bd068c
Richard <cyanyao@live.com>
2019-02-26 10:49:14 +0800

    add config file

---
+++ src/App.config.js
@@ -0,0 +1,7 @@
+const remoteHost = 'http://localhost:7001/demo'
+const paymentPath = '/payment'
+
+export const api = {
+  // api of payment data manipulation
+  paymentData: `${remoteHost}${paymentPath}`,
+}
