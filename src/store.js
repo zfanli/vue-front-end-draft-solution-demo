f55a86ae6726fcb0cd1d5166234ba9000d1aea26
Richard <cyanyao@live.com>
2019-02-26 17:15:47 +0800

    remove test data and add fetch api

--- src/store.js
+++ src/store.js
@@ -1,26 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { api } from './App.config'
+import Axios from 'axios'
 
 Vue.use(Vuex)
-
-// This is a test data,
-// it will be deleted while layout is done
-const pd = [
-  {
-    vendor: 'Test Vendor',
-    registeredMonth: new Date('2019-01'),
-    requestStartDate: new Date('2019-01-22'),
-    requestEndDate: new Date('2019-01-23'),
-    payStatus: '',
-    mediaName: 'Test Media Name',
-    itemName: 'Test Item',
-    channel: '0',
-    registeredStartDate: new Date('2019-01-25'),
-    registeredEndDate: new Date('2019-01-26'),
-    price: 123,
-    paidAmount: 109,
-  },
-]
 
 export default new Vuex.Store({
   state: {
@@ -28,15 +11,47 @@ export default new Vuex.Store({
     message: '',
     form: '',
     vendorOptions: [],
-    payStatusOptions: [],
+    payStatusOptions: [
+      {
+        label: '未支払',
+        value: '0',
+      },
+      {
+        label: '支払済',
+        value: '1',
+      },
+    ],
     loading: false,
-    paymentData: pd,
+    paymentData: [],
   },
   mutations: {
+    // for test error box,
+    // trigger display of error box
     error(state, message) {
-      state.error = true
-      state.message = message
+      if (state.error) {
+        state.error = false
+      } else {
+        state.error = true
+        state.message = message
+      }
+    },
+    paymentData(state, pd) {
+      state.paymentData = pd
+    },
+    loading(state, flag) {
+      state.loading = flag
     },
   },
-  actions: {},
+  actions: {
+    getPaymentData({ commit }, filter) {
+      Axios.get(api.paymentData, {
+        params: filter,
+      }).then(({ data }) => {
+        const result = {}
+        data.map((t, i) => (result[i] = t))
+        commit('paymentData', result)
+        commit('loading', false)
+      })
+    },
+  },
 })
